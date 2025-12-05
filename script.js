603579a6bf84e93615ff22b1f94a7c11a4fa1526
Victor <victorsouzavieira.22@gmail.com>
2025-12-05 01:30:53 -0300

    Arrumando algumas coisas

--- script.js
+++ script.js
@@ -1,48 +1,23 @@
-class MobileNavBar {
-    constructor(mobileMenu, navList, navLinks) {
-       this.mobileMenu = document.querySelector(mobileMenu);
-       this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-
-    handleClick() {
-        this.navList.classList.toggle(this.activeClass);
-
-    }
-
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-
-    init() {
-        if (this.mobileMenu) {
-            this.addClickEvent();
+$(document).ready(function () {
+  $('.block-tickets__content-pack').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.slick-prev-custom'),
+    nextArrow: $('.slick-next-custom'),
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
         }
-        return this;
-    }
-}
-const mobileNavBar = new MobileNavBar(
-    ".mobileMenu",
-    ".navList",
-    ".navList li"
-);
-
-mobileNavBar.init();
-
-if ($(window).width() <= 1028) {
-    $('.alignContainer').slick({
-      slidesToShow: 1, // n
-      slidesToScroll: 1,
-      arrows: true,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
-      nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
-
-    });
-    
-  }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+});
