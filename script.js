6fd5b7f7a5fa657c0b6c30c0404f1d4281cd3352
Victor <victorsouzavieira.22@gmail.com>
2026-01-20 18:21:11 -0300

    install slick

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-$(document).ready(function () {
-  $('.block-tickets__content-pack').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: $('.slick-prev-custom'),
-    nextArrow: $('.slick-next-custom'),
-    responsive: [
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-        }
-      }
-    ]
+$(document).ready(function(){
+  $('.block-tickets__slider').slick({
+    setting-name: setting-value
   });
 });
